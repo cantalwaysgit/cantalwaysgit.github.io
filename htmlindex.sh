0b4c784be861a562b40781f9f639356a618bb560
pipe'n'play <wordsnbees@mail.org>
2022-05-20 01:09:53 +0200

    index html without index script

--- htmlindex.sh
+++ htmlindex.sh
@@ -1,1 +1,1 @@
-ls -R | awk 'BEGIN { out = 1 } $0 ~ /^\.\/lingoriver/ || $0 ~ /^\.\/subtitle/ { out = 0 } $0 ~ /^\s*$/ { out = 1 } out == 1 { print }' | htmllinkls | my-txt2html 
+ls -R | awk 'BEGIN { out = 1 } $0 ~ /^\.\/lingoriver/ || $0 ~ /^\.\/subtitle/ || $0 ~ /^htmlindex/ { out = 0 } $0 ~ /^\s*$/ { out = 1 } out == 1 { print }' | htmllinkls | my-txt2html 
